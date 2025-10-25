525cd45c6db948392d29242f064710f252e62980
kkamaDev <kkama3326@gmail.com>
2025-10-25 21:58:09 +0500

    MoveZero algaritmi

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,5 @@
-# nums = int(input("son kriiting >>"))
-# sum = 0
-# for num in str(nums):
-#     sum = sum + int(num)
-# print(sum)
 
-
-# # sonni max  va min topish
-# nums = [1,5,3,7,9,6,4]
-# max_s = nums[0]
-# min_s = nums[0]
-# for i in nums:
-#     if i < min_s:
-#         min_s = i
-#     if i > max_s:
-#         max_s = i 
-# print(f"min {min_s}")
-# print(f"max : {max_s}")
-
-
-
-
-
-
-# # nollarni ortga surish
+# nollarni ortga surish
 
 # def moveZero(nums:list):
 #     count = 0
@@ -35,12 +12,3 @@
 # son = [0,1,2,0,12]
 # print(moveZero(son))
 
-
-
-def teskari(x):
-    teskari = 0
-    oxirgi = x % 10
-    teskari = teskari * 10 + oxirgi
-    x = x // 10
-    return x
-print(teskari(123))
