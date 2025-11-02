8c7b1944b1ffecd4a12868f4c44fa6333e1b9c1f
kkamaDev <kkama3326@gmail.com>
2025-11-02 10:56:10 +0500

    Create maxArea.py

---
+++ maxArea.py
@@ -0,0 +1,13 @@
+def maxArea(nums: list) -> int:
+  i , j = 0 , len(nums) -1 # ro'yxatni boshi va oxirini olamiz 
+  max_area = 0  # yzuamizni eng kattasini saqlab boramiz va qaytaramiz 
+  while i < j:  # index lar  bir briga teng bo'lib qolsa ular yaqinlashib bitta ustunga teng bo'lib qoladi
+    area = (j-i) * min(nums[i] , nums[j]) #   (j-i) enini qaytaradi min(nums[i] , nums[j]) eng kichik devor uzunligigacha to'ladi
+    max_area = max(area , max_area) # agara area miz katta bo'lsa qiymat yangilab qo'yamiz 
+    if nums[i] > nums[j]:  # ustunlarni siljitamiz agar boshidagi qiymatlar katta bo'lsa ro'yxatni oxiridan qsqartramiz 
+        j-=1
+    else:
+        i+=1
+  return max_area 
+
+print(maxArea([1,8,6,2,5,4,8,3,7]))
