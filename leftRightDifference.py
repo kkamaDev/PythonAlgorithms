345dd282767899446ebcf85be1caf2f1ee1bfa2d
kkamaDev <kkama3326@gmail.com>
2025-11-02 23:02:12 +0500

    Update leftRightDifference.py

--- leftRightDifference.py
+++ leftRightDifference.py
@@ -1,20 +1,26 @@
-def sign(num):
-    if num > 0:
-        return 1
-    elif num < 0:
-        return -1
-    return 0
-
-
-
 def LeftRightDiffirence(nums:list)->list:
     left,right  = 0 , sum(nums) 
     result = []
     for num in nums:
         left+=num
-        result.append(sign(right-left))
         
+        if right - left > 0:
+            result.append(1)
+        elif right - left < 0:
+            result.append(-1)
+        else:
+            result.append(0)
         right-=num
     return result
-print(LeftRightDiffirence([1,2,3,3]))
+print(LeftRightDiffirence([1,2,3,4]))
+
+
+# yoki bo'lmasa kodni chiroyliroq qilish uchun sign deb alohida funksiya bilan tekshirish mumkin
+
+# def sign(num):
+#     if num  > 0:
+#         return 1
+#     elif num < 0:
+#         return -1
+#     return 0 
     
